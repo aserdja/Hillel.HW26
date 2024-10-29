ae4fa14b436f5bd419984a5163bac549d7ccf6c2
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-10-29 21:08:14 +0200

    Added ObservableCollection and methods

---
+++ HW26/HW26/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HW26
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var libra = new Library();
+			libra.AvailableBooksCollection.CollectionChanged += (s, e) => Console.WriteLine($"{e.Action} a book!");
+			libra.Add(new Book { Title = "TestBook", ISBN = "123-456" });
+			libra.Remove("123-456");
+		}
+	}
+}
