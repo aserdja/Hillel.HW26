0b5ad35a1578f61d2ae3c35ca653d065cade7011
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-10-29 19:49:02 +0200

    Created Book props

--- HW26/HW26/Book.cs
+++ HW26/HW26/Book.cs
@@ -2,5 +2,9 @@
 {
 	public class Book
 	{
+		public string Title { get; set; } = string.Empty;
+		public string Author { get; set; } = string.Empty;
+		public int Year { get; set; }
+		public string ISBN { get; set; } = string.Empty;
 	}
 }
